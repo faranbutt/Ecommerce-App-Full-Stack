b26ac4ecca69c273b5d8a8f818b81f4084786cdf
faranbutt <faranbutt789@gmail.com>
2023-06-11 19:52:24 +0500

    pushing af

--- src/app/Female/page.tsx
+++ src/app/Female/page.tsx
@@ -4,8 +4,9 @@ import { client } from "../../../sanity/lib/client";
 import { urlForImage } from "../../../sanity/lib/image";
 import Link from "next/link";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { useEffect,useRef } from "react";
 import VanillaTilt from "vanilla-tilt";
+
 
 export type IProducts = {
   _id: string;
@@ -17,41 +18,21 @@ export type IProducts = {
     name: string;
   };
 };
-
 const fetchProducts = async () => {
-  const data: IProducts[] = await client.fetch(
-    `*[_type=='product' && category -> name == 'Female']{
+  const data: IProducts[] =
+    await client.fetch(`*[_type=='product' && category -> name == 'Female']{
       _id,
       title,
       description,
       price,
-      image,
-      category -> {
-        name
-      }
-    }`
-  );
+        image,
+        category -> {
+          name
+        }
+    }`);
   return data;
 };
 
-const options = {
-  scale: 1.2,
-  speed: 1000,
-  max: 30,
-};
-
-function Tilt(props: any) {
-  const { options, ...rest } = props;
-  const tilt = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    if (tilt.current) {
-      VanillaTilt.init(tilt.current, options);
-    }
-  }, [options]);
-
-  return <div ref={tilt} {...rest} />;
-}
 
 export default async function page() {
   const femaleProducts: IProducts[] = await fetchProducts();
@@ -59,27 +40,26 @@ export default async function page() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 mx-5 md:mx-20 gap-y-10 md:gap-x-20 text-center">
       {femaleProducts.map((item) => (
-        <Tilt className="box" options={options} key={item._id}>
-          <div key={item._id} className="rounded-3xl border-4 border-secondary">
-            <Link href={`/${item.title}`}>
-              <Image
-                src={urlForImage(item.image).url()}
-                alt="person"
-                width={400}
-                height={400}
-              />
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                {item.title}
-              </h4>
-              <p className="scroll-m-20 text-lg font-semibold tracking-tight text-gray-400">
-                {item.description}
-              </p>
-              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                {item.price}$
-              </h4>
-            </Link>
-          </div>
-        </Tilt>
+        
+        <div key={item._id} className="rounded-3xl border-4 border-secondary">
+          <Link href={`/${item.title}`}>
+            <Image
+              src={urlForImage(item.image).url()}
+              alt="person"
+              width={400}
+              height={400}
+            />
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+              {item.title}
+            </h4>
+            <p className="scroll-m-20 text-lg font-semibold tracking-tight text-gray-400">
+              {item.description}
+            </p>
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+              {item.price}$
+            </h4>
+          </Link>
+        </div>
       ))}
     </div>
   );
