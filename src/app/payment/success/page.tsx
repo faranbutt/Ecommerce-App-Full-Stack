7ac6ace59ae96afa7daaba8be46443043acfe05f
faranbutt <faranbutt789@gmail.com>
2023-06-20 19:09:58 +0500

    Fixed some bugs

---
+++ src/app/payment/success/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Stripe from 'stripe'
+
+export default function page() {
+  return (
+    <div className='flex justify-center items-center'>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Order Successfull.😋
+      </h1>
+    </div>
+  )
+}
